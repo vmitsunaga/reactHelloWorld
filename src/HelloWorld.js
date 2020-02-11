17d3e842801902ceb70e0d47b071b6ff1a18b375
Vitor Mitsunaga <vitor.mitsunaga@gmail.com>
2020-02-11 17:42:27 -0300

    finished first program

---
+++ src/HelloWorld.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './HelloWorld.css';
+
+class HelloWorld extends Component {
+  constructor(props) {
+    super (props);
+    this.state = { greeting: 'Hello' };
+    this.frenchify = this.frenchify.bind(this);
+    this.removeGreeting = this.removeGreeting.bind(this);
+  }
+
+  frenchify() {
+    this.setState({ greeting: 'Bonjour' });
+  }
+
+  removeGreeting() {
+    this.props.removeGreeting(this.props.name);
+  }
+
+  render() {
+    return (
+      <div className="HelloWorld">
+        {this.state.greeting} {this.props.name}!
+        <br/>
+        <button onClick={this.frenchify}>Frenchfy!</button>
+        <br/>
+        <button onClick={this.removeGreeting}>Remove Me!</button>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld;
